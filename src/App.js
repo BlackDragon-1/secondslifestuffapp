7156d13da6b4ee434fc6856b16b1433ba7c51488
BlackDragon-1 <michal.piotr.marczynski@gmail.com>
2021-02-15 10:37:01 +0100

    add components Home Login Register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'; 
+import Home from './components/home/home';
+import Login from './components/login/login';
+import Register from './components/register/register';
+
 
 function App() {
-  return (
+  return (<>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+        <h1> Hello world </h1>
+        <Home/>
+        <Login/>
+        <Register/>
+        
+      
     </div>
+    </>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            komponent Home
+        </div>
+    );
+};
+
+export default Home;
